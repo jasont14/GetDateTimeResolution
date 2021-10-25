074366096d6fc99c13298f3def965c4501eaa945
Jason Thatcher <contact@jasonthatcher.com>
2021-10-25 12:34:39 -0500

    program cs file.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Timespan will set 5-seconds
+            TimeSpan duration = TimeSpan.FromSeconds(5);                        
+
+            //distinctValues will hold the number of result sets from call to DateTime.UtcNow
+            var distinctValues = new HashSet<DateTime>();
+
+
+            //Stopwatch to run for duration and collect distinctValues
+            var stopWatch = Stopwatch.StartNew();
+            while (stopWatch.Elapsed < duration)
+            {
+                distinctValues.Add(DateTime.UtcNow);
+            }
+
+            //Display count and accuracy by dividing 5-seconds by count
+            Console.WriteLine("Sample Count: " + distinctValues.Count);
+            Console.WriteLine($"Accuracy: {stopWatch.Elapsed.TotalMilliseconds / distinctValues.Count:0.000000} ms");
+            
+        }
+    }
+}
